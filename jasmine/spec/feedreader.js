5732991e9162e29955435dab97ee53b433ca8dd2
Jun Clemente <junclemente@outlook.com>
2017-03-11 15:34:29 -0800

    style: Cleaned up js files

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,16 +3,15 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -25,32 +24,27 @@ $(function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('are defined and url is defined', function() {
+        it('are defined and url is defined', function() {
             for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i]).toBeDefined();
                 expect(allFeeds[i].url).toBeDefined();
             }
-         });
-
+        });
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it('have defined names and that name is not empty', function() {
-            for (var i = 0;i < allFeeds.length; i++) {
+        it('have defined names and that name is not empty', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name).toMatch(/\w/);
             }
-         });
+        });
     });
-
-
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
@@ -58,30 +52,26 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-         it('is hidden by default', function() {
+        it('is hidden by default', function() {
             expect($('body')).toHaveClass('menu-hidden');
-         });
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-          it('changes visibility when clicked and hides when clicked again', function() {
+        });
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+        it('changes visibility when clicked and hides when clicked again', function() {
             var spyEvent = spyOnEvent('.menu-icon-link', 'click');
-
             $('.menu-icon-link').click();
             expect('click').toHaveBeenTriggeredOn('.menu-icon-link');
             expect(spyEvent).toHaveBeenTriggered();
             expect($('body')).not.toHaveClass('menu-hidden');
-
             $('.menu-icon-link').click();
             expect('click').toHaveBeenTriggeredOn('.menu-icon-link');
             expect(spyEvent).toHaveBeenTriggered();
             expect($('body')).toHaveClass('menu-hidden');
-          });
+        });
     });
-
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
@@ -90,72 +80,46 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
-         // The following test was used to check that async call is being performed and that
-         // the test would fail because it was not accounted for.
-         // it('should fail because not accounting for asynchrounous call', function() {
-         //    loadFeed(0);
-         //    expect($('.entry-link')).not.toBeInDOM();
-         // });
-
-         beforeEach(function(done) {
-            loadFeed(0,function() {
+        // The following test was used to check that async call is being performed and that
+        // the test would fail because it was not accounted for.
+        // it('should fail because not accounting for asynchrounous call', function() {
+        //    loadFeed(0);
+        //    expect($('.entry-link')).not.toBeInDOM();
+        // });
+        beforeEach(function(done) {
+            loadFeed(0, function() {
                 done();
             });
-         });
-
-         it('should have at least a single entry in entry within the feed container', function(done) {
-            loadFeed(0);
+        });
+        it('should have at least a single entry in entry within the feed container', function(done) {
             expect($('.entry-link')).toBeInDOM();
             done();
-         });
-
-
+        });
     });
-
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-
-         var originalTimeout, feedList, firstFeed, secondFeed;
-         var feed0 = 0;
-         var feed1 = 1;
-         var feed2 = 2;
-         var feed3 = 3;
-
-         beforeAll(function() {
+        var originalTimeout, feedList, firstFeed, secondFeed;
+        var FEED0 = 0,
+            FEED1 = 1,
+            FEED2 = 2,
+            FEED3 = 3;
+        beforeAll(function(done) {
             originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
             jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-
-            loadFeed(feed0, function() {
+            loadFeed(FEED0, function() {
                 feedList = $('.feed').find('.entry-link');
                 firstFeed = feedList[0].innerText;
                 console.log(firstFeed);
                 expect(firstFeed).toBeDefined();
                 done();
             });
-         });
-
-         // beforeEach(function(done) {
-
-         // });
-
-         it('ensures that the content changes when a new feed is loaded', function(done) {
-            loadFeed(feed1, function() {
-                feedList = $('.feed').find('.entry-link');
-                secondFeed = feedList[0].innerText;
-                console.log(secondFeed);
-                expect(secondFeed).toBeDefined();
-                expect(firstFeed).not.toEqual(secondFeed);
-                done();
-            });
-         });
-
+        });
         it('ensures that the content changes when a new feed is loaded', function(done) {
-                loadFeed(feed2, function() {
+            loadFeed(FEED1, function() {
                 feedList = $('.feed').find('.entry-link');
                 secondFeed = feedList[0].innerText;
                 console.log(secondFeed);
@@ -163,10 +127,9 @@ $(function() {
                 expect(firstFeed).not.toEqual(secondFeed);
                 done();
             });
-         });
-
+        });
         it('ensures that the content changes when a new feed is loaded', function(done) {
-                loadFeed(feed3, function() {
+            loadFeed(FEED2, function() {
                 feedList = $('.feed').find('.entry-link');
                 secondFeed = feedList[0].innerText;
                 console.log(secondFeed);
@@ -174,10 +137,19 @@ $(function() {
                 expect(firstFeed).not.toEqual(secondFeed);
                 done();
             });
-         });
-
+        });
+        it('ensures that the content changes when a new feed is loaded', function(done) {
+            loadFeed(FEED3, function() {
+                feedList = $('.feed').find('.entry-link');
+                secondFeed = feedList[0].innerText;
+                console.log(secondFeed);
+                expect(secondFeed).toBeDefined();
+                expect(firstFeed).not.toEqual(secondFeed);
+                done();
+            });
+        });
         afterAll(function() {
             jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
         });
-     });
+    });
 }());
